5e15e747817d408842198a265831375a3dcefba2
Genaro08 <genaro.08@hotmail.es>
2020-09-18 17:29:00 -0300

    Agrego el archivo modificado del repositorio clonado

--- Tester.java
+++ Tester.java
@@ -1,6 +1,6 @@
 public class Tester {
 	public static void main(String[] args) {
-		Persona sujeto1 = new Persona("Sasuke");
+		Persona sujeto1 = new Persona("Naruto");
 		Mascota mascota1 = new Mascota("Pakkun");
 		sujeto1.setMascota(mascota1);
 		System.out.println("El nombre del sujeto 1 es "+sujeto1.getNombre()+" y su mascota es "+sujeto1.getMascota().getNombre());
